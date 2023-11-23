465bf13024ea348fbdc92c2ed6937afb7466d9ea
yujinlee <now8567@hanmail.net>
2023-11-24 00:08:47 +0900

    dfs 추가

--- src/algorithm03.java
+++ src/algorithm03.java
@@ -10,6 +10,8 @@ public class algorithm03 {
 
     static int[] dx = {1 , 0 , -1 , 0};
     static int[] dy = {0 , 1 , 0 , -1};
+    static int sum = 1;
+    static List<Integer> list = new ArrayList<>();
 
 
     public static void main(String[] args) {
@@ -30,11 +32,18 @@ public class algorithm03 {
                 {1,1,1,0,0},
                 {0,0,0,0,1}
         };
-        System.out.println(solution(inputArr2));
+        System.out.println(solution(inputArr));
+//        System.out.println(solution(inputArr2));
 
     }
 
     public static int solution(int[][] maps) {
+//        int answer = getBfsAnswer(maps);
+        int answer = getDfsAnswer(maps);
+        return answer;
+    }
+
+    private static int getBfsAnswer(int[][] maps) {
         int[][] visited =  new int[maps.length][maps[0].length];
         visited[0][0]= 1;
 
@@ -44,8 +53,6 @@ public class algorithm03 {
         if(answer == 0){
             answer = -1;
         }
-
-
         return answer;
     }
 
@@ -77,6 +84,35 @@ public class algorithm03 {
             }
         }
     }
+
+    private static int getDfsAnswer(int[][] maps) {
+        int[][] visited =  new int[maps.length][maps[0].length];
+        visited[0][0]= 1;
+
+        return dfs(0, 0, maps, visited);
+
+    }
+
+
+    public static int dfs(int cX, int cY, int[][] maps, int[][] visited){
+
+        if (cX == maps.length - 1 && cY == maps[0].length - 1) {
+            return sum;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int mX = cX + dx[i];
+            int mY = cY + dy[i];
+            if (mX >= 0 && mX < maps.length && mY >= 0 && mY < maps[0].length && maps[mX][mY] == 1 && visited[mX][mY] == 0 ) {
+                visited[mX][mY] = 1;
+                sum ++;
+                return dfs(mX, mY, maps, visited);
+            }
+        }
+        return -1;
+
+    }
+
 
 
 }
